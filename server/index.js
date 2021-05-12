7f1284743f968635dd8d97d1aaa9cb97c27f9bdc
Nadiasina588 <nadiasinanicodev@gmail.com>
2021-05-12 15:50:30 +0300

    add functionality backend chat

--- server/index.js
+++ server/index.js
@@ -9,6 +9,24 @@ const router = require('./router');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+io.on('connection', (socket) => {
+  console.log('We have a new connection !!!');
+
+  socket.on('join', ({ name, room }, callback) => {
+    console.log(name, room);
+    const error = true;
+    if(error) {
+      callback({error: 'error'});
+
+    }
+   
+  })
+
+  socket.on('disconnect', () => {
+    console.log('User had left!!!');
+  })
+})
+
 
 app.use(router);
 
